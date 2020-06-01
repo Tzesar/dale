a0e59c603e1ecc230ce647ec873bd1592ff4a87f
Augusto Amarilla <agu.amarilla@gmail.com>
2020-05-31 20:14:10 -0400

    Added TypeORM files

---
+++ database.ts
@@ -0,0 +1,57 @@
+import { Connection, ConnectionManager, ConnectionOptions, createConnection, getConnectionManager } from 'typeorm'
+import 'envkey'
+
+/**
+ * Database manager class
+ */
+export class Database {
+    private connectionManager: ConnectionManager
+
+    constructor() {
+        this.connectionManager = getConnectionManager()
+    }
+
+    public async getConnection(): Promise<Connection> {
+        const CONNECTION_NAME = `default`
+
+        let connection: Connection
+
+        if (this.connectionManager.has(CONNECTION_NAME)) {
+            console.info(`Database.getConnection()-using existing connection ...`)
+            connection = await this.connectionManager.get(CONNECTION_NAME)
+
+            if (!connection.isConnected) {
+                connection = await connection.connect()
+            }
+        }
+        else {
+            console.info(`Database.getConnection()-creating connection ...`)
+
+            const connectionOptions: ConnectionOptions = {
+                name: `default`,
+                type: `mysql`,
+                port: 3306,
+                synchronize: true,
+                logging: true,
+                host: 'localhost',
+                username: 'elumen',
+                database: 'dale',
+                password: 'elumen',
+                entities: [
+                    __dirname + "/entities/*.*"
+                ]
+            }
+
+            // Don't need a pwd locally
+            // if (process.env.DB_PASSWORD) {
+            //     Object.assign(connectionOptions, {
+            //         password: process.env.DB_PASSWORD
+            //     })
+            // }
+
+            connection = await createConnection(connectionOptions)
+        }
+
+        return connection
+    }
+}
---
+++ entities/user.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    constructor(id: number, name: string) {
+        this.id = id;
+        this.name = name;
+    }
+}
